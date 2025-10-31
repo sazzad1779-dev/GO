58bb03ca3aa76a6e9160ef3ef38523477f4224ac
sazzad1779-dev <sazzad1779@gmail.com>
2025-11-01 01:25:07 +0600

    Implement code changes to enhance functionality and improve performance

--- chapter2/type_casting.go
+++ chapter2/type_casting.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello")
+	var a int = 10
+	var b float32 = 11.1
+	var c int = a + int(b)
+	var d float32 = float32(a) + b
+	fmt.Println("a: ", a, "\nb: ", b, "\nint total :", c, "\nfloat32 total", d)
 }
